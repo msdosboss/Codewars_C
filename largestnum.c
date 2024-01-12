aea98a133117951f40b47fd3492eefcf1274d4f6
msdosboss <jacobt0722@gmail.com>
2024-01-12 09:53:19 -0800

    working on largestnum.c still still

--- largestnum.c
+++ largestnum.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <math.h>
 
+
+
+/*TODO Change ii into a dynmically sized array of longs because ii as a single long gets to large to fit in the 8 bytes that it is given so we are working on the fact that a^4 * a^4 = a^8 there by allowing us to seperate the pow() calculations into multiple parts*/
+
 int last_digit(const char *a, const char *b){
     printf("\na = %s\n", a);
     printf("\nb = %s\n", b);
@@ -21,11 +25,16 @@ int last_digit(const char *a, const char *b){
     if((aa == 0 && bb[1] == '\0' && bb[0] == 0) || (bb[1] == '\0' && bb[0] == 0))
         return 1;
     int prod = 1;
-    int ii = 1;
+    long ii = 1;
     while(1){
+        printf("\nii = %d\n", ii);
         printf("bb = %s\n", bb);
         if((bb[lenbb - 1] - '0') % 2 == 1){
-            printf("\ni: %d\n", ii);
+            printf("\nii = %d\n",ii);
+            /*for(int i = 0; i < ii; i++){
+                prod = prod * aa;
+                prod = prod % 10;
+            }*/
             prod = (prod * pow(aa,ii));
             prod = prod % 10;
         }
@@ -36,15 +45,17 @@ int last_digit(const char *a, const char *b){
                 bb[i+1] = bb[i+1] + 5;
             bb[i] = ((bb[i] - '0') / 2) + '0';
         }
-        int lenbbb = 0;                     //this will be the amount of spaces we are getting rid of 
-        char *bbb;
+        
+        int lenbbb = 0;                     //this will be the amount of spaces we are getting rid of
         for(int i = 0;; i++){
             if(bb[i] != '0')
                 break;
-            printf("Im real\n");
+            printf("shortning the string\n");
             lenbbb++;
         }
-        if(lenbb != 0){
+        printf("this is bb before get shrunk %s\n", bb);
+        if(lenbb != 0){ 
+            char *bbb;
             bbb = malloc(sizeof(char) * (lenbb - lenbbb) + 1);
             strcpy(bbb,&bb[lenbbb]);
             bb = realloc(bb, sizeof(char) *(lenbb - lenbbb) + 1);
@@ -62,7 +73,8 @@ int last_digit(const char *a, const char *b){
 
 
 void main(){
-    printf("%d",1 / 2);
+    printf("\n%d\n", sizeof(long));
+    //printf("\n%d\n",last_digit("1606938044258990275541962092341162602522202993782792835301376","2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376"));
 }
 
 
@@ -75,5 +87,5 @@ void main(){
 
 //b = 2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376
 //bb = 9223372036854775807
-//10000000000
-//5000000000
+//144115188075855872
+// 720575
